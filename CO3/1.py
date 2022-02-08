4d01b4a0df63b839264d37a9bb938a309102963d
marvin thomas <91838044+marvin2255@users.noreply.github.com>
2022-02-08 15:59:49 +0530

    Add files via upload

---
+++ CO3/1.py
@@ -0,0 +1,3 @@
+import math
+print(math.pow(9, 3))
+print (math.gcd(12, 36))
---
+++ CO3/PackageCO2.py
@@ -0,0 +1,12 @@
+from Graphics import Circle
+from Graphics import Rectangle
+from Graphics._3DGraphics import Cuboid
+from Graphics._3DGraphics import Sphere
+Circle.Area(1)
+Circle.Perimeter(1)
+Rectangle.Area(1,1)
+Rectangle.Perimeter(1,1)
+Cuboid.Area(1,1,1)
+Cuboid.Perimeter(1,1,1)
+Sphere.Area(1)
+Sphere.Perimeter(1)
